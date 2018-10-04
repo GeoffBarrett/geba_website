626759823e0645c260898b037738850ecf140b9a
unknown <geoffrey.m.barrett@gmail.com>
2018-10-03 20:24:48 -0400

    modifying wsgi to match the original

--- geba_website/config/wsgi.py
+++ geba_website/config/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-# os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.staging"
+from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-from django.core.wsgi import get_wsgi_application, WSGIHandler
 application = get_wsgi_application()
-# application = WSGIHandler()
