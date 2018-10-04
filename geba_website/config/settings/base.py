9f9f7bdadd6338d9398099d84f11971389d270c0
unknown <geoffrey.m.barrett@gmail.com>
2018-10-03 22:52:16 -0400

    adding a fullpath to the secrets.json file so I stop getting errors about its location'

--- geba_website/config/settings/base.py
+++ geba_website/config/settings/base.py
@@ -12,9 +12,33 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from sys import path
 from unipath import Path
-from .utils import get_env_variable, get_secret
+import json
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(var_name):
+    try:
+        # return os.environ[var_name]
+        return os.environ.get(var_name)
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 
 DJANGO_ROOT = Path(__file__).ancestor(3)  # /geba_website/geba_website
+
+secrets_filename = os.path.join(DJANGO_ROOT, 'secrets.json')
+
+with open(secrets_filename) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % setting
+        raise ImproperlyConfigured(error_msg)
+
 
 CONFIG_ROOT = DJANGO_ROOT.child("config")
 
